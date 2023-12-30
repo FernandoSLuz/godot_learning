bcc177ccd942c5185808e0317ec43d71c0e88bf1
FernandoSLuz <fernandosilvadaluz@gmail.com>
2023-12-30 14:38:22 -0500

    created behavior for shooting laser and grenades with custom scenes.

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -9,21 +9,28 @@ var can_grenade: bool = true
 func _process(_delta):
 	#input
 	var direction = Input.get_vector("left","right","up","down")
-	#position += direction * 500 * delta
 	velocity = direction * 500
 	move_and_slide()
 	
 	#lase input
 	if Input.is_action_just_pressed("primary_action") and can_laser:
-		on_laser_shot.emit(self)
+		var selected_laser_respawn = randomize_respawn($WeaponStartPositions)
+		on_laser_shot.emit(self, selected_laser_respawn.global_position)
 		can_laser = false
-		$laser_timer.start(0.5)
+		$LaserTimer.start(0.5)
 		
 	#grenade input
 	if Input.is_action_just_pressed("secondary_action") and can_grenade:
-		on_grenade_shot.emit(self)
+		var selected_grenade_respawn = randomize_respawn($WeaponStartPositions)
+		on_grenade_shot.emit(self, selected_grenade_respawn.global_position)
 		can_grenade = false
-		$grenade_timer.start(2.0)
+		$GrenadeTimer.start(2.0)
+
+func randomize_respawn(node: Node2D) -> Marker2D:
+	var respawns = node.get_children()
+	var respawn = respawns[randf_range(0, respawns.size())]
+	return respawn
+	
 
 func _on_laser_timer_timeout():
 	can_laser = true
