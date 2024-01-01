eb3fcfb31ed7e37960aa12e66dede9dd1e04833d
FernandoSLuz <fernandosilvadaluz@gmail.com>
2024-01-01 17:27:16 -0500

    adjusted drone moving script

--- scenes/drone/drone.gd
+++ scenes/drone/drone.gd
@@ -1,28 +1,12 @@
 extends CharacterBody2D
 
-var drone_speed: int = 500
-var drone_image: Sprite2D
-var drone_size: Vector2
-
-func _ready():
-	drone_image = $Sprite2D
-	drone_size = drone_image.get_rect().size
-	respawn_drone()
-
-func _process(delta):
-	move_drone(delta)
-	
-func move_drone(delta):
+func _process(_delta):
+	#direction
 	var direction = Vector2.RIGHT
-	
-	velocity = direction * drone_speed
-	
+	#velocity
+	velocity = direction * 200
+	#move and slide
 	move_and_slide()
 	
-	if position.x > get_viewport_rect().size.x+(drone_size.x/2):
-		respawn_drone()
-
-func respawn_drone():
-	var viewport_rect = get_viewport_rect()
-	var random_y = randf_range(0+(drone_size.y/2), viewport_rect.size.y-(drone_size.y/2))
-	position = Vector2(0-(drone_size.x/2), random_y)
+func hit():
+	print('got hit')
