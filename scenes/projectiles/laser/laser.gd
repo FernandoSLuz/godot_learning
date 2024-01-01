5b5f86fcfdffe10c9cf79dc7dd34184934864bc2
FernandoSLuz <fernandosilvadaluz@gmail.com>
2024-01-01 17:27:48 -0500

    added self destruct and hit destruct.

--- scenes/projectiles/laser/laser.gd
+++ scenes/projectiles/laser/laser.gd
@@ -5,3 +5,15 @@ var direction: Vector2 = Vector2.UP
 
 func _process(delta):
 	position += direction * speed * delta
+
+
+func _on_body_entered(body):
+	$SelfDestructTimer.stop()
+	if body.has_method("hit"): #you can also use 'if "hit" in body:'
+		body.hit()
+	queue_free()
+
+
+func _on_timer_timeout():
+	print('stoped')
+	queue_free()
