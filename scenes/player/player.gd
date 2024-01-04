e92408f23d3dabb9f055f70f4504044c53c9564d
FernandoSLuz <fernandosilvadaluz@gmail.com>
2024-01-03 19:52:26 -0500

    updated var names.

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -23,9 +23,8 @@ func _process(_delta):
 	
 	#lase input
 	var player_direction = (get_global_mouse_position() - position).normalized()
-	if Input.is_action_just_pressed("primary_action") and can_laser and Globals.laser_amount > 0:
-		Globals.laser_amount -= 1
-		print('ammo left: ', Globals.laser_amount)
+	if Input.is_action_just_pressed("primary_action") and can_laser and Globals.laser_current_amount > 0:
+		Globals.laser_current_amount -= 1
 		$GPUParticles2D.emitting = true
 		var selected_laser_respawn = randomize_respawn($WeaponStartPositions)
 		on_laser_shot.emit(selected_laser_respawn.global_position, player_direction)
@@ -33,8 +32,8 @@ func _process(_delta):
 		$LaserTimer.start(0.5)
 		
 	#grenade input
-	if Input.is_action_just_pressed("secondary_action") and can_grenade and Globals.grenade_amount > 0:
-		Globals.grenade_amount -= 1
+	if Input.is_action_just_pressed("secondary_action") and can_grenade and Globals.grenade_current_amount > 0:
+		Globals.grenade_current_amount -= 1
 		var selected_grenade_respawn = randomize_respawn($WeaponStartPositions)
 		on_grenade_shot.emit(selected_grenade_respawn.global_position, player_direction)
 		can_grenade = false
