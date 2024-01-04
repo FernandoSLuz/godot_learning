74fe55e488f078db757527a2c46226a7d5eb94a6
FernandoSLuz <fernandosilvadaluz@gmail.com>
2024-01-03 19:52:10 -0500

    added max_value and setting the current on _init
    the reason is that _init is the first function to run (even befor _ready)

--- globals/globals.gd
+++ globals/globals.gd
@@ -1,4 +1,12 @@
 extends Node
 
-var laser_amount = 20
-var grenade_amount = 5
+
+var laser_max_amount: float = 20
+var laser_current_amount: float
+var grenade_max_amount: float = 5
+var grenade_current_amount: float
+
+func _init():
+	laser_current_amount = laser_max_amount
+	grenade_current_amount = grenade_max_amount
+
