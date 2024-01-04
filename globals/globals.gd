4fe9f39a1405e5805cfebd4861082145aafb8fd1
FernandoSLuz <fernandosilvadaluz@gmail.com>
2024-01-04 18:52:09 -0500

    update initial value of ammo

--- globals/globals.gd
+++ globals/globals.gd
@@ -2,11 +2,6 @@ extends Node
 
 
 var laser_max_amount: float = 20
-var laser_current_amount: float
+var laser_current_amount: float = 10
 var grenade_max_amount: float = 5
-var grenade_current_amount: float
-
-func _init():
-	laser_current_amount = laser_max_amount
-	grenade_current_amount = grenade_max_amount
-
+var grenade_current_amount: float = 3
