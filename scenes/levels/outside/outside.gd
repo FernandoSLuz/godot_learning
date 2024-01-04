16a05e7b20f9889fe2e8e198ac67377d56aa00d2
FernandoSLuz <fernandosilvadaluz@gmail.com>
2024-01-04 18:54:13 -0500

    updated level with pickup items, and also added signal on level scene to update

--- scenes/levels/outside/outside.gd
+++ scenes/levels/outside/outside.gd
@@ -4,6 +4,3 @@ func _on_gate_player_entered_gate(_body):
 	var tween = create_tween()
 	tween.tween_property($Player, "speed", 0, 0.5)
 	TransitionLayer.change_scene("res://scenes/levels/inside/inside.tscn")
-
-func _on_gate_player_exited_gate(body):
-	print("body has exited: ", body)
