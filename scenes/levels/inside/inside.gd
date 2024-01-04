6712ef4d2ab17430a244ba5ff09209fcb74fa233
FernandoSLuz <fernandosilvadaluz@gmail.com>
2024-01-03 20:21:52 -0500

    changed change_scene call on inside and outside levels

--- scenes/levels/inside/inside.gd
+++ scenes/levels/inside/inside.gd
@@ -5,5 +5,6 @@ func _on_exit_gate_area_body_entered(_body):
 	var tween = create_tween()
 	tween.tween_property($Player, "speed", 0, 0.5)
 	#get_tree().change_scene_to_file("res://scenes/levels/outside/outside.tscn")
-	get_tree().change_scene_to_packed(outside_level_scene)
+	TransitionLayer.change_scene(outside_level_scene)
+	#get_tree().change_scene_to_packed(outside_level_scene)
 	
