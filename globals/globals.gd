78e09ee006227878f8bb96806a2a443c21a9877f
FernandoSLuz <fernandosilvadaluz@gmail.com>
2024-01-04 19:59:57 -0500

    added GetSet for globals, instead of multi signals

--- globals/globals.gd
+++ globals/globals.gd
@@ -1,7 +1,27 @@
 extends Node
 
+signal stat_change
 
 var laser_max_amount: float = 20
-var laser_current_amount: float = 10
+var laser_current_amount: float = 10:
+	get:
+		return laser_current_amount
+	set(value):
+		laser_current_amount = value
+		stat_change.emit()
+
 var grenade_max_amount: float = 5
-var grenade_current_amount: float = 3
+var grenade_current_amount: float = 3:
+	get:
+		return grenade_current_amount
+	set(value):
+		grenade_current_amount = value
+		stat_change.emit()
+
+var health_max_amount: float = 100
+var health_current_amount: float = 60:
+	get:
+		return health_current_amount
+	set(value):
+		health_current_amount = value
+		stat_change.emit()
