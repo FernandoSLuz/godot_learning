f23854ae6e36ead2afbd9850d24c0df6ebd3aa08
FernandoSLuz <fernandosilvadaluz@gmail.com>
2024-01-04 21:11:38 -0500

    connecting with group signals and using deferred calls.

--- scenes/levels/level.gd
+++ scenes/levels/level.gd
@@ -3,6 +3,17 @@ class_name LevelParent
 
 var laser_scene: PackedScene = preload("res://scenes/projectiles/laser/laser.tscn")
 var grenade_scene: PackedScene	= preload("res://scenes/projectiles/grenade/grenade.tscn")
+var item_scene: PackedScene = preload("res://scenes/Items/item.tscn")
+
+func _ready():
+	for container in get_tree().get_nodes_in_group('Container'):
+		container.connect("open", _on_container_opened)
+
+func _on_container_opened(pos, direction):
+	var item = item_scene.instantiate()
+	item.position = pos
+	item.direction = direction
+	$Items.call_deferred('add_child', item)
 
 func _on_player_laser_shot(respawn_position: Vector2, laser_direction: Vector2):
 	var laser = laser_scene.instantiate() as Area2D
