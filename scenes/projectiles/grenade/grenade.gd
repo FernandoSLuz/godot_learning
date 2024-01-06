e2ff1b5f9505c82ae396160562b5522bd8ace775
FernandoSLuz <fernandosilvadaluz@gmail.com>
2024-01-06 17:14:27 -0500

    bugfix of get groups
    we need to get groups every frame, or else it will try to get a position of an already destroyed object.

--- scenes/projectiles/grenade/grenade.gd
+++ scenes/projectiles/grenade/grenade.gd
@@ -6,27 +6,21 @@ const speed = 1500
 
 var exploding: bool = false
 var explosion_radius: int = 400
-var containers: Array[Node]
-var entities: Array[Node]
-
 
 func _ready():
 	physics_material_override.bounce = grenade_bounce_normalized
 	linear_damp = grenade_linear_damp
-	containers = get_tree().get_nodes_in_group('Container')
-	entities = get_tree().get_nodes_in_group('Entities')
 	
 func _process(_delta):
 	if exploding:
 		calculate_targets_distance()
 
 func calculate_targets_distance():
-	var hitable_objects = containers + entities
+	var hitable_objects = get_tree().get_nodes_in_group('Container') + get_tree().get_nodes_in_group('Entities')
 	for hitable_object in hitable_objects:
 		var in_range:bool  = hitable_object.global_position.distance_to(global_position) < explosion_radius
 		if in_range and "hit" in hitable_object:
 			hitable_object.hit()
-
 
 func explode():
 	$AnimationPlayer.play("explosion")
