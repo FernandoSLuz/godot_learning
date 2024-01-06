7fd1486f782ebc7aedc2873f2265618ea4fece8c
FernandoSLuz <fernandosilvadaluz@gmail.com>
2024-01-06 12:36:52 -0500

    fixed on_house_enter/exit

--- scenes/levels/outside/outside.gd
+++ scenes/levels/outside/outside.gd
@@ -4,3 +4,12 @@ func _on_gate_player_entered_gate(_body):
 	var tween = create_tween()
 	tween.tween_property($Player, "speed", 0, 0.5)
 	TransitionLayer.change_scene("res://scenes/levels/inside/inside.tscn")
+
+
+func _on_house_player_entered():
+	var tween = get_tree().create_tween()
+	tween.tween_property($Player/Camera2D, "zoom", Vector2(1,1), 1).set_trans(Tween.TRANS_SINE)
+
+func _on_house_player_exited():
+	var tween = get_tree().create_tween()
+	tween.tween_property($Player/Camera2D, "zoom", Vector2(0.6,0.6), 1).set_trans(Tween.TRANS_SINE)
