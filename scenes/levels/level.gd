15c435c719087f7f505c12fb12570b7b808b8ee5
FernandoSLuz <fernandosilvadaluz@gmail.com>
2024-01-06 12:37:14 -0500

    removed old code

--- scenes/levels/level.gd
+++ scenes/levels/level.gd
@@ -21,30 +21,9 @@ func _on_player_laser_shot(respawn_position: Vector2, laser_direction: Vector2):
 	laser.rotation_degrees = rad_to_deg(laser_direction.angle())+90
 	laser.direction = laser_direction
 	$Projectiles.add_child(laser)
-	$UI.update_laser_text()
 
 func _on_player_grenade_shot(respawn_position: Vector2, grenade_direction: Vector2):
 	var grenade = grenade_scene.instantiate() as RigidBody2D
 	grenade.position = respawn_position
 	grenade.linear_velocity = grenade_direction * grenade.speed
 	$Projectiles.add_child(grenade)
-	$UI.update_grenade_text()
-
-
-func _on_house_player_entered():
-	var tween = get_tree().create_tween()
-	tween.set_parallel(true)
-	tween.tween_property($Player, "modulate:a", 0,1)
-	tween.tween_property($Player/Camera2D, "zoom", Vector2(1,1), 1).set_trans(Tween.TRANS_SINE)
-
-
-func _on_house_player_exited():
-	var tween = get_tree().create_tween()
-	tween.set_parallel(true)
-	tween.tween_property($Player, "modulate:a", 1,1)
-	tween.tween_property($Player/Camera2D, "zoom", Vector2(0.6,0.6), 1).set_trans(Tween.TRANS_SINE)
-
-
-func _on_player_update_stats():
-	$UI.update_laser_text()
-	$UI.update_grenade_text()
