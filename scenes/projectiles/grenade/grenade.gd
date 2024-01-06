e97e7ecfa55c72b543527b86dd7d2105904fe397
FernandoSLuz <fernandosilvadaluz@gmail.com>
2024-01-06 16:12:56 -0500

    added grenade hit script

--- scenes/projectiles/grenade/grenade.gd
+++ scenes/projectiles/grenade/grenade.gd
@@ -4,10 +4,30 @@ const speed = 1500
 @export_range(0, 1, 0.01) var grenade_bounce_normalized: float = 0.15
 @export_range(-1, 100, 0.1) var grenade_linear_damp: float = 1.6
 
+var exploding: bool = false
+var explosion_radius: int = 400
+var containers: Array[Node]
+var entities: Array[Node]
+
 
 func _ready():
 	physics_material_override.bounce = grenade_bounce_normalized
 	linear_damp = grenade_linear_damp
+	containers = get_tree().get_nodes_in_group('Container')
+	entities = get_tree().get_nodes_in_group('Entities')
+	
+func _process(_delta):
+	if exploding:
+		calculate_targets_distance()
+
+func calculate_targets_distance():
+	var hitable_objects = containers + entities
+	for hitable_object in hitable_objects:
+		var in_range:bool  = hitable_object.global_position.distance_to(global_position) < explosion_radius
+		if in_range and "hit" in hitable_object:
+			hitable_object.hit()
+
 
 func explode():
 	$AnimationPlayer.play("explosion")
+	exploding = true
