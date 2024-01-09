4eed3edc9bbc4693d2942ca64799115a12911b5b
FernandoSLuz <fernandosilvadaluz@gmail.com>
2024-01-08 19:12:59 -0500

    updated level with drone.

--- scenes/levels/outside/outside.gd
+++ scenes/levels/outside/outside.gd
@@ -5,7 +5,6 @@ func _on_gate_player_entered_gate(_body):
 	tween.tween_property($Player, "speed", 0, 0.5)
 	TransitionLayer.change_scene("res://scenes/levels/inside/inside.tscn")
 
-
 func _on_house_player_entered():
 	var tween = get_tree().create_tween()
 	tween.tween_property($Player/Camera2D, "zoom", Vector2(1,1), 1).set_trans(Tween.TRANS_SINE)
