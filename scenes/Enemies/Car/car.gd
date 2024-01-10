f3ac4b6d0050da0654dc5c679a36913fcc591677
FernandoSLuz <fernandosilvadaluz@gmail.com>
2024-01-09 19:43:52 -0500

    created turret follow mechanichs, alongside with raycast

--- scenes/Enemies/Car/car.gd
+++ scenes/Enemies/Car/car.gd
@@ -1,16 +1,42 @@
 extends PathFollow2D
 
 var player_near: bool = false
+
+@onready var ray1: Line2D = $Turret/Canon1/Ray1
+@onready var ray2: Line2D = $Turret/Canon2/Ray2
+
+@onready var fire1: Sprite2D = $Turret/Canon1/Fire1
+@onready var fire2: Sprite2D = $Turret/Canon2/Fire2
+
+func fire():
+	Globals.health_current_amount -= 20
+	fire1.modulate.a = 1
+	fire2.modulate.a = 1
+	var tween = create_tween()
+	tween.set_parallel(true)
+	tween.tween_property(fire1, "modulate:a", 0, randf_range(0.1,0.5))
+	tween.tween_property(fire2, "modulate:a", 0, randf_range(0.1,0.5))
+
+func _ready():
+	ray2.add_point($Turret/Canon2.target_position)
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	#progress_ratio += 0.00 * delta
+	progress_ratio += 0.025 * delta
 	if player_near:
-		#Globals.look_at_smoothly($Turret, Globals.player, delta)
 		$Turret.look_at(Globals.player.position)
-
-
-func _on_notice_area_body_entered(body):
+	
+func _on_notice_area_body_entered(_body):
+	$AnimationPlayer.stop()
+	$AnimationPlayer.play("laser_load")
 	player_near = true
 
-func _on_notice_area_body_exited(body):
+func _on_notice_area_body_exited(_body):
+	$AnimationPlayer.pause()
+	var tween = create_tween()
+	tween.set_parallel(true)
+	tween.tween_property(ray1, "width", 0, randf_range(0.1,0.5))
+	tween.tween_property(ray2, "width", 0, randf_range(0.1,0.5))
+	await tween.finished
+	$AnimationPlayer.stop()
 	player_near = false
