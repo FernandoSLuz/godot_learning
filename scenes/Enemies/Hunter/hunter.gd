ea0245e2db9e338a957588a301a79422132cbdc7
FernandoSLuz <fernandosilvadaluz@gmail.com>
2024-01-10 19:32:04 -0500

    tweak on pathfinding

--- scenes/Enemies/Hunter/hunter.gd
+++ scenes/Enemies/Hunter/hunter.gd
@@ -10,10 +10,12 @@ func _ready():
 func _physics_process(_delta):
 	if(active):
 		var next_path_pos: Vector2 = $NavigationAgent2D.get_next_path_position()
+		print(next_path_pos)
 		var direction: Vector2 = (next_path_pos - global_position).normalized()
 		velocity = direction * speed
 		move_and_slide()
 		look_at(Globals.player.global_position)
+		_on_navigation_timer_timeout()
 
 
 func _on_notice_area_body_entered(_body):
