a38c1718b85d10215574c4f2e758c52865ff7f4d
FernandoSLuz <fernandosilvadaluz@gmail.com>
2024-01-11 20:51:34 -0500

    laser and grenade sfx

--- scenes/projectiles/laser/laser.gd
+++ scenes/projectiles/laser/laser.gd
@@ -3,6 +3,9 @@ extends Area2D
 @export var speed: int = 1000
 var direction: Vector2 = Vector2.UP
 
+func _ready():
+	$AudioStreamPlayer2D.play()
+
 func _process(delta):
 	position += direction * speed * delta
 
